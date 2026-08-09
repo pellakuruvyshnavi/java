24124401163d2c79e22094364611d8007938007f
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-08-09 21:44:37 +0530

    Add files via upload

---
+++ Giantwheel.java
@@ -0,0 +1,32 @@
+package day4;
+
+import java.util.Scanner;
+
+public class Giantwheel {
+    public static void main(String[] args) {
+         Scanner scan = new Scanner(System.in);
+        for(int cabin=91;cabin>=87;cabin--){
+            System.out.println("New cabin arrived");
+        for(int person=1;person<=4;) {
+            System.out.println("let me Know age ");
+            int age = scan.nextInt();
+            // person =(age>=18&&age<=60)?person+1:person;
+            if(age>=18&&age<=60){
+                person++;
+                System.out.println("Enjoy the ride");
+
+            }
+            else 
+                System.out.println("Safety is prior over ride");
+        }   
+        }
+     scan.close();
+
+
+
+
+
+
+    } 
+
+}
