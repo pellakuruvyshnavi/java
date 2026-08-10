9e8002901780404a2915dbb839b854be1c84ba01
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-08-10 21:41:50 +0530

    Add files via upload

---
+++ DemoMultiLevel.java
@@ -0,0 +1,17 @@
+package day11;
+import java.util.Arrays;
+import java,util,Comparator;
+  public class DemoMultiLevel{
+         public static void main(string[] arr){
+             Executive ex = new Executive();
+             ex.viewGoods();
+             ex.alter();
+             ex.lenSort();
+ }
+}
+ class Executive extends Modify{
+         public void lenSort(){
+          System.out.println("\n\nSort by No of chars");
+            Arrays.stream(goods).sorted(Comparator.comparing(String::length)).forEach(System.out::println);
+}
+}
