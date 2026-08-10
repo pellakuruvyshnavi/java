c6eb3f7f267b1e96cfaa1353b31baa5e35018dce
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-08-10 21:41:08 +0530

    Add files via upload

---
+++ DemoFactorPattern.java
@@ -0,0 +1,14 @@
+package day11;
+import java.util.Arrays;
+import java.util.Comparator;
+ public class DemoFactoryPattern{
+  Warehouse house = new DemoSingle();//factory pattern upcasting
+  house.viewGoods();
+// house.illustrateReference();//even though DemoSingle constructor it has casted to warehouse
+ house=new Modify();
+System.out.println("\n\nReverse Sorting via comparator");
+Arrays.stream(house.goods).sorted(Comparator.reverseOrder())
+.forEach(System.out::println);
+}
+
+}
