79e8c68e6a139401471572ac8335dc49a2f675ad
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-08-11 15:13:48 +0530

    Add files via upload

---
+++ paaing.java
@@ -0,0 +1,34 @@
+package day5;
+
+import java.util.Arrays;
+
+public class paaing {
+    public void suggest (double[] list){
+        for(double each:list){
+            if(each>=4.5&&each<=10)
+                System.out.println("Eligiblev 7L in 15% ROI");
+            else if (each>10&&each<=25.1){
+                System.out.println("Business loan 20l 8%");
+
+            }
+            else{
+                System.out.println("pl 2l in 27% interst");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        itr =(itr>12&&itr<=20)?itr-=(itr*0.10):
+        (itr>20&&itr<=25)?itr-=(itr*0.20):
+        (itr>25)?itr-=(itr*0.300):itr;
+        System.out.println("Deuducted"+itr);
+
+    }public static void ordering (double[] salary) {
+        Arrays.sort(salary);
+
+        
+    }
+    public static void main(String[] args) {
+        double[] annual
+    }
+}
+
