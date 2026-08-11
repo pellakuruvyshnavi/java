b16879d743cf357f1ee56e48642044ef9138330b
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-08-11 15:11:40 +0530

    Add files via upload

---
+++ TwistedPrime.java
@@ -0,0 +1,22 @@
+package day4;
+
+import java.util.Scanner;
+
+public class TwistedPrime {
+    public static void main(String[] args) {
+        Scanner get = new Scanner(System.in);
+        int number =0;rev =0;
+        System.out.println( "Enter the number");
+        number = get.nextInt();
+        while (number>0) {
+            rev=rev*10+(number%10);
+            number/=10;
+            
+        }
+        for(int times =1; times<=2;times++){;di
+            for(int div=2;div<=Math.sqrt(number))
+        }
+    }
+    
+    
+}
