0faeffe1216b4f71dd06e4295cdf3cb1731a7f5e
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-08-12 21:04:58 +0530

    Add files via upload

---
+++ maximumandminimumvalue.java
@@ -0,0 +1,32 @@
+package day7;
+
+import java.util.Arrays;
+
+public class maximumandminimumvalue {
+    public static int[] find(int[][] arr){
+        int[] result = {
+            Integer.MAX_VALUE,
+            Integer.MIN_VALUE
+        };
+        for(int[] row:arr){
+            result[0] =Math.min(result[0],
+                Arrays.stream(row).
+               min().getAsInt());
+
+               result[1] = Math.max(result[1],
+                 Arrays.stream(row).
+              max().getAsInt());
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[][] tds = {
+            {2,5,12},
+            {9,4},
+            {45,89,20,30}
+        };
+        int[] received = find(tds);
+        System.out.println(Arrays.toString(received));
+        
+    }
+}
