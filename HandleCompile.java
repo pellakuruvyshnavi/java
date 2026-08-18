b659a14e77e9744bf8683e41cb016f5936ad7c88
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-08-18 14:07:03 +0530

    Add files via upload

---
+++ HandleCompile.java
@@ -0,0 +1,16 @@
+package day13;
+public class HandleCompile{
+    
+ public static void main(String[] args){
+      String[] algorithms ={ "Two pointers","DFS","BFS","Intervals","Djikstra",prims};
+     Arrays.steam(algorithms).forEach(each->{
+        System.out.println(each);
+      try{ 
+     Thread.sleep(5000);
+}
+catch(InterruptedException e){
+      e.printStackTrace();
+}
+});
+}
+}
