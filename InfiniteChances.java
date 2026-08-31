3151a1ad92933c2d51bbe2a3f63719b163456338
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-08-31 16:49:33 +0530

    Add files via upload
    infinite chances

---
+++ InfiniteChances.java
@@ -0,0 +1,26 @@
+package day13;
+import java.util.Scanner;
+
+public class InfiniteChances {
+    public static void handling(){
+        int start =0, end =0;
+        Scanner scan = new Scanner (System.in);
+        String text = "vyshnavi is a good girl ";
+        try{
+            System.out.println("start, end pos to extract");
+            start =scan.nextInt();
+            end =scan.nextInt();
+            System.out.println(text .substring(start,end));
+
+        } catch(StringIndexOutOfBoundsException sin){
+            System.out.println("between 0 and "+text.length());
+            handling();
+        }
+        scan.close();
+    }
+    public static void main(String[] args) {
+        handling();
+    }
+    
+}
+
